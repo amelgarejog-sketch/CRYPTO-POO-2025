374b0cbca368e792064041075eb6163fac900332
JDGB11 <juandiegogranadosbernal@gmail.com>
2025-12-07 09:57:41 -0500

    Implement abstract Entity class with hitbox management

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,1 +1,31 @@
+package entities;
 
+import java.awt.geom.Rectangle2D;
+
+public abstract class Entity {
+    protected float x, y;
+    protected int width, height;
+    protected Rectangle2D.Float hitbox;
+
+    public Entity(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        initHitbox();
+    }
+
+    protected void initHitbox() {
+        // El hitbox podría ser más pequeño que el sprite visual
+        hitbox = new Rectangle2D.Float(x, y, width, height);
+    }
+
+    protected void updateHitboxPosition() {
+        hitbox.x = x;
+        hitbox.y = y;
+    }
+
+    public Rectangle2D.Float getHitbox() { return hitbox; }
+    public float getX() { return x; }
+    public float getY() { return y; }
+}
